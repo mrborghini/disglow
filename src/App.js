3cdc8241658a8fefa8998d22f9cb5527b6b0665c
anerivs <aneri.shah@nyu.edu>
2024-09-28 18:06:44 -0400

    added color slider-needs to be fixed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { ColorSlider, parseColor } from '@react-spectrum/color'; // Import parseColor
+import { Provider, defaultTheme } from '@adobe/react-spectrum';
 
 function App() {
+  // Use parseColor to set an initial color value
+  const [color, setColor] = useState(parseColor('#703a1d'));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider theme={defaultTheme}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Edit <code>src/App.js</code> and save to reload.</p>
+
+          {/* Use the ColorSlider */}
+          <ColorSlider
+            value={color}
+            onChange={setColor}
+            channel="lightness"
+            label="Select your skin tone"
+          />
+
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </Provider>
   );
 }
 
